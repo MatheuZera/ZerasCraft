0b4160dc04cdc613eaffc1a86d50daf39d7087c0
MatheuZera <138898955+MatheuZera@users.noreply.github.com>
2025-07-07 14:01:09 -0300

    f

--- js/music-player.js
+++ js/music-player.js
@@ -19,7 +19,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // --- Playlist de Músicas ---
     const playlist = [
-        { title: "Aria-Math", src: "audios/musics/Aria-Math.mp3" },
+        { title: "Aria-Math", src: "audios/musics/Aria-Math-Lofi-Remake.mp3" },
+        { title: "Aria-Math-Lofi", src: "audios/musics/Aria-Math.mp3" },
         { title: "Beginning 2", src: "audios/musics/Beginning-2.mp3" },
         { title: "Biome-Fest", src: "audios/musics/Biome-Fest.mp3" },
         { title: "Blind-Spots", src: "audios/musics/Blind-Spots.mp3" },
