f2047eb981653b1a35aefa88a4fbd6c92627672a
MatheuZera <138898955+MatheuZera@users.noreply.github.com>
2025-09-23 14:43:51 -0300

    g

--- backups/primário/script.js
+++ backups/primário/script.js
@@ -238,7 +238,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Seletores para os elementos
     const cardElements = document.querySelectorAll(
-        '.service-card, .role-category-card, .access-card, .community-card, .event-card, .security-card, .faq-item, .info-card, .card, .marketplace-item, .wiki-category-card, .article-card, .youtube-card, .server-card, .donation-tier-card, .vote-site-card, .team-member-card, .news-featured-card, .news-article-card, .job-opening-card, .forum-post-card, .comment-card, .stat-item, .parallax-card, .card-container, .result-card'
+        '.service-card, .role-category-card, .access-card, .community-card, .event-card, .security-card, .faq-item, .info-card, .card, .marketplace-item, .wiki-category-card, .article-card, .youtube-card, .server-card, .donation-tier-card, .vote-site-card, .team-member-card, .news-featured-card, .news-article-card, .job-opening-card, .forum-post-card, .comment-card, .stat-item, .parallax-card, .card-container, .result-card, card-compact'
     );
 
     const buttonElements = document.querySelectorAll(
